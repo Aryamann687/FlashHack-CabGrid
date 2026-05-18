53fdd3ed5babf0fa7b20029cd428345ae811d3f1
Aryamann Srivastava <aryamann20@gmail.com>
2026-05-18 11:00:13 +0530

    Added Ui file

---
+++ ui.py
@@ -0,0 +1,10 @@
+def print_header():
+
+    print("=" * 60)
+    print("        CABGRID DISPATCH OPTIMIZER")
+    print("=" * 60)
+
+
+def section(title):
+
+    print(f"\n{'='*20} {title} {'='*20}")
